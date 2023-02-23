0b10014bb7324fbef43e3db51480b253f3355128
Code Sandbox <hello@codesandbox.io>
2023-02-23 22:07:45 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1ccb39143b7b17292ae73f8497d22294.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec83dec58d60d27d7eca45ac6e50f95",
+    "url": "https://vineethkuddiagana.github.io/csb-s2qobd/index.html"
+  },
+  {
+    "revision": "7441b963c4cd19361e97",
+    "url": "https://vineethkuddiagana.github.io/csb-s2qobd/static/js/2.71074f19.chunk.js"
+  },
+  {
+    "revision": "5ead9a4d0d6b30295f91",
+    "url": "https://vineethkuddiagana.github.io/csb-s2qobd/static/js/main.657ed050.chunk.js"
+  },
+  {
+    "revision": "6368d5c32dcad53d4b7a",
+    "url": "https://vineethkuddiagana.github.io/csb-s2qobd/static/js/runtime-main.2b075d15.js"
+  }
+]);
